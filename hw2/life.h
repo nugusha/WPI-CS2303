443f2ade7bf9c37049b00d927d617900aadd6f28
nugusha <n.u.g.u.s.h.a.a@gmail.com>
2017-02-01 21:06:31 -0500

    Create life.h

---
+++ hw2/life.h
@@ -0,0 +1,11 @@
+/*
+ * life.h
+ * Created by Nugzar Chkhaidze nchkhaidze
+ */
+
+#ifndef LIFE_H_
+#define LIFE_H_
+#include<stdio.h>
+void PlayOne(int rows,int columns,int *A[],int *B[],int gens);
+void print_array(int rows,int columns,int *A[]);
+#endif /* LIFE_H_ */
