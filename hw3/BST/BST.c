330320861025a8af0057c50bbc919b5550aeb27e
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-02-03 23:45:17 -0500

    Update BST.c

--- hw3/BST/BST.c
+++ hw3/BST/BST.c
@@ -7,27 +7,20 @@ extern int Total;
 FILE *out;
 FILE **in;
 int main(int argc, char *argv[]) {
-
 	out=fopen(argv[1],"w");
-
 	in=malloc((argc-2)*sizeof(FILE));
 
 	for(int i=2;i<argc;i++){
 		in[i-2]=fopen(argv[i],"r");
 	}
-
 	char *word;
 	word=malloc(100*sizeof(char));
-
 
 	char ***words=(char ***)malloc(sizeof(char**)*(argc-2));
 	int *wordsize=malloc(sizeof(int)*(argc-2));
 
-
 	for(int i=0;i<argc-2;i++){
-
 		int counter=0,p=1;
-
 		while(1){
 			p=fscanf(in[i],"%s",word);
 			if(p!=1)break;
@@ -39,8 +32,6 @@ int main(int argc, char *argv[]) {
 		wordsize[i]=counter;
 		rewind(in[i]);
 
-
-
 		int N=0;
 		p=1;
 		while(1){
@@ -48,32 +39,21 @@ int main(int argc, char *argv[]) {
 
 			if(p!=1)break;
 
-		//	if(strcmp(word, change(word)))printf("%d === |%s| |%s| %d %d\n",N,word,change(word),strlen(word),strlen(change(word)));
-
 			word=strdup(change(word));
 			if(strlen(word)==0)continue;
 
 			N++;
 			words[i][N-1] = strdup(word);
-		//	printf("%d === %s\n",N,words[i][N-1]);
-
 		}
 	}
-
 	process(words,argc-2,wordsize,out);
-
-
 
 	for(int i=0;i<argc-2;i++){
 		for(int j=0;j<wordsize[i];j++)
 			free(words[i][j]);
 		free(words[i]);
 	}
-	free(words);
-
-	free(word);
-
-	free(wordsize);
+	free(words);free(word);free(wordsize);
 
 	for(int i=0;i<argc-2;i++)
 		free(in[i]);
