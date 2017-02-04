9bc3ed60570182be66cb3478dc0829935ba08317
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-02-03 22:22:29 -0500

    Create BST.c

---
+++ hw3/BST/BST.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "process.h"
+extern int Total;
+FILE *out;
+FILE **in;
+int main(int argc, char *argv[]) {
+
+	out=fopen(argv[1],"w");
+
+	in=malloc((argc-2)*sizeof(FILE));
+
+	for(int i=2;i<argc;i++){
+		in[i-2]=fopen(argv[i],"r");
+	}
+
+	char *word;
+	word=malloc(100*sizeof(char));
+
+
+	char ***words=(char ***)malloc(sizeof(char**)*(argc-2));
+	int *wordsize=malloc(sizeof(int)*(argc-2));
+
+
+	for(int i=0;i<argc-2;i++){
+
+		int counter=0,p=1;
+
+		while(1){
+			p=fscanf(in[i],"%s",word);
+			if(p!=1)break;
+			word=strdup(change(word));
+			if(strlen(word)>0)
+				counter++,Total++;
+		}
+		words[i]=(char **)malloc(sizeof(char*)*counter);
+		wordsize[i]=counter;
+		rewind(in[i]);
+
+
+
+		int N=0;
+		p=1;
+		while(1){
+			p=fscanf(in[i],"%s",word);
+
+			if(p!=1)break;
+
+		//	if(strcmp(word, change(word)))printf("%d === |%s| |%s| %d %d\n",N,word,change(word),strlen(word),strlen(change(word)));
+
+			word=strdup(change(word));
+			if(strlen(word)==0)continue;
+
+			N++;
+			words[i][N-1] = strdup(word);
+		//	printf("%d === %s\n",N,words[i][N-1]);
+
+		}
+	}
+
+	process(words,argc-2,wordsize,out);
+
+
+
+	for(int i=0;i<argc-2;i++){
+		for(int j=0;j<wordsize[i];j++)
+			free(words[i][j]);
+		free(words[i]);
+	}
+	free(words);
+
+	free(word);
+
+	free(wordsize);
+
+	for(int i=0;i<argc-2;i++)
+		free(in[i]);
+	free(in);
+	free(final);
+
+	return 0;
+}
