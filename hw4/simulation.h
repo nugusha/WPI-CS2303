d9620541d635121e02118c83a9e134ba19e03bd6
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-02-14 04:15:37 -0500

    Create simulation.h

---
+++ hw4/simulation.h
@@ -0,0 +1,16 @@
+/*
+ * simulation.h
+ *
+ *  Created on: Feb 14, 2017
+ *      Author: Nugzar Chkhaidze & Daniel Sullivan
+ */
+
+#ifndef SIMULATION_H_
+#define SIMULATION_H_
+
+void solve1(int customers,int tellers);
+void solve2(int customers,int tellers);
+void print();
+
+
+#endif /* SIMULATION_H_ */
