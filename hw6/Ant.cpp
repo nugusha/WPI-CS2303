00010b27f778cad0294282ae0ff5f0491ed04219
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-03-02 14:42:54 -0500

    Update Ant.cpp

--- hw6/Ant.cpp
+++ hw6/Ant.cpp
@@ -1,5 +1,5 @@
 /*
- * Ant.h
+ * Ant.cpp
  *
  *  Created on: Feb 27, 2017
  *      Author: Nugzar Chkhaidze & Daniel Sullivan
@@ -8,25 +8,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#ifndef ANT_H_
-#define ANT_H_
-
+#include "Ant.h"
+#include "board.h"
 #include "Organism.h"
 
-class Ant:public Organism {
-public:
-	Ant(int X,int Y);
-	virtual ~Ant(){}
+using namespace std;
 
-	void print();
-	int type();
-	Organism* getnew();
+//extern int gridsize;
+extern vector<pair<int,int> >moves;
 
-	pair<int,int> move(Organism*** A, Organism*** B);
-	void breed(Organism*** A, Organism*** B);
-	int time_to_bread();
+Ant::Ant(int X,int Y) {
+	x=X;
+	y=Y;
+}// end Ant
 
-};
+void Ant::print(){
+	cout<<"o";
+	return;
+}// end print
+int Ant::type(){
+	return 1;
+}// end type
+pair<int,int> Ant::move(Organism*** curBoard, Organism*** newBoard){
 
-#endif /* ANT_H_ */
+	random_shuffle(moves.begin(),moves.end());
+	for(int i=0;i<4;i++){	// check surroundings
+		int newx=x+moves[i].first;
+		int newy=y+moves[i].second;
+
+		if(!check(newx,newy))continue;
+
+		if(curBoard[newx][newy]->type()>0 ||
+		   newBoard[newx][newy]->type()!=0){
+			continue;  // not empty
+		}else{
+				newBoard[newx][newy]=new Ant(newx,newy);
+				newBoard[newx][newy]->timer=timer+1;
+
+				return make_pair(x,y);
+			}
+		}
+
+
+	return make_pair(-1,-1);
+}// end move
+Organism* Ant::getnew(){
+	Ant* a=new Ant(x,y);
+	a->timer=timer;
+	return a;
+}// end getnew
+int Ant::time_to_bread(){
+	return(timer>=3);
+}// end time_to_breed
+void Ant::breed(Organism*** curBoard, Organism*** newBoard){
+
+	random_shuffle(moves.begin(),moves.end());
+	random_shuffle(moves.begin(),moves.end());
+	for(int i=0;i<4;i++){	// check surroundings
+		int newx=x+moves[i].first;
+		int newy=y+moves[i].second;
+
+		if(!check(newx,newy))continue;
+
+		if(newBoard[newx][newy]->type()==0){
+				newBoard[newx][newy]=new Ant(newx,newy);
+				newBoard[newx][newy]->timer=0;
+				timer=0;
+				return ;
+			}
+		}
+
+
+
+	return;
+}// end breed
