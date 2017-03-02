09589e9a73eb05281ed4a6f18c7382c3d8560a5b
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-03-01 22:15:35 -0500

    Add files via upload

---
+++ hw6/Organism.cpp
@@ -0,0 +1,21 @@
+/*
+ * Organism.cpp
+ *
+ *  Created on: Feb 27, 2017
+ *      Author: Nugzar Chkhaidze & Daniel Sullivan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "Organism.h"
+using namespace std;
+
+Organism::Organism() {
+
+}
+void Organism::print(){
+	cout<<".";
+	return;
+}
