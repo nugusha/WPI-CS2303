a29b650842ebf4060d1fa45f9262c3d5d9a42dce
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-03-02 14:44:02 -0500

    Update Doodlebug.h

--- hw6/Doodlebug.h
+++ hw6/Doodlebug.h
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-
+using namespace std;
 #include "Organism.h"
 
 #ifndef DOODLEBUG_H_
@@ -19,19 +19,17 @@ public:
 	Doodlebug(int X,int Y);
 	virtual ~Doodlebug(){}
 
+
 	int type();
 	void print();
-	void move() {
+	pair<int,int> move(Organism*** curBoard, Organism*** newBoard);
+	pair<int,int> tryeat(Organism*** curBoard, Organism*** newBoard);
 
-	}
+	void starvation(Organism*** curBoard);
+	int time_to_bread();
+	Organism* getnew();
 
-	void starvation() {
-
-	}
-
-	void breed() {
-
-	}
+	void breed(Organism*** curBoard, Organism*** newBoard);
 };
 
 #endif /* DOODLEBUG_H_ */
