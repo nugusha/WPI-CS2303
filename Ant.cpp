7b822e35c6cd2d75a8aaeec965148f72b5344fec
Nugzar Chkhaidze <nchkhaidze@wpi.edu>
2017-03-01 22:17:16 -0500

    Add files via upload

---
+++ Ant.cpp
@@ -0,0 +1,44 @@
+/*
+ * Ant.cpp
+ *
+ *  Created on: Feb 27, 2017
+ *      Author: Nugzar Chkhaidze & Daniel Sullivan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "Ant.h"
+
+using namespace std;
+
+extern int gridsize;
+
+Ant::Ant(int X,int Y) {
+	x=X;
+	y=Y;
+}
+
+void Ant::print(){
+	cout<<"o";
+	return;
+}
+int Ant::type(){
+	return 1;
+}
+void Ant::move(){
+
+
+
+
+	return;
+}
+void Ant::breed(){
+
+
+
+
+
+	return;
+}
